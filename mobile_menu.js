8448586b7c556f610e2eb7d43757ba666e4158f7
Ryan Wagner <rwagner@spyderbytedesign.com>
2013-05-09 17:45:10 -0400

    Added functionality to allow for automatic hiding of menu titles above breakpoint.

--- mobile_menu.js
+++ mobile_menu.js
@@ -2,20 +2,30 @@
   Drupal.behaviors.mobile_menu = {
     attach: function(context, settings) {
 
+      //Get current width and set variables to avoid incredibly verbose functions.
       var currentWidth = getwidth();
       var breakpoint = Drupal.settings.mobile_menu.menu_breakpoint;
       var menu_ids = Drupal.settings.mobile_menu.menu_ids;
+      var title_hide = Drupal.settings.mobile_menu.hide_title;
+
 
       //On document load, check to see if at the mobile breakpoint. If so, trigger mobile mode.
       if (currentWidth <= breakpoint) {
         mobilizeMenus(menu_ids);
       };
 
+      //On document load, if title hide is true and we're above the breakpoint, hide menu titles.
+      if (currentWidth > breakpoint && title_hide == 1) {
+        $.each( menu_ids, function(key, value) {
+          $(value + " h2").hide();
+        });
+      };
+
       //Hides menus and binds click to show.
       function mobilizeMenus(menu_array) {
         $.each( menu_array, function(key, value) {
           $(value + " .menu").hide();
-          $(value + " h2").bind('click', function() {
+          $(value + " h2").show().bind('click', function() {
             $(value + " .menu").slideToggle();
           });
         });
@@ -26,6 +36,9 @@
         $.each( menu_array, function(key, value) {
           $(value + " .menu").show();
           $(value + " h2").unbind('click');
+          if (title_hide == 1) {
+            $(value + " h2").hide();
+          };
         });
       }
 
@@ -47,7 +60,7 @@
         return viewportwidth;
       }
 
-      //Monitors browser resizing and modifies menus if the breakpoint is crossed. Does not fire if the breakpoint wasn't crossed.
+      //Monitors browser resizing and modifies menus if the breakpoint is crossed. Does not trigger mobilizeMenus/desktopMenus if the breakpoint wasn't crossed.
       $(window).resize(function() {
         if ((getwidth() > breakpoint && currentWidth <= breakpoint) || (getwidth() <= breakpoint && currentWidth >= breakpoint)) {
           currentWidth = getwidth();
